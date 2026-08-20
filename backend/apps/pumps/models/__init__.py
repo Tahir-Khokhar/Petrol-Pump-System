a199542c629ba921753d116a8845865072b5c981
Tahir-Khokhar <khokhartahir290@gmail.com>
2026-08-20 17:57:58 +0500

    Add remaining app files and configurations

---
+++ backend/apps/pumps/models/__init__.py
@@ -0,0 +1,5 @@
+from apps.pumps.models.pump import Pump  # noqa: F401
+from apps.pumps.models.pump_fuel_type import PumpFuelType  # noqa: F401
+from apps.pumps.models.nozzle import Nozzle  # noqa: F401
+
+__all__ = ['Pump', 'PumpFuelType', 'Nozzle']
