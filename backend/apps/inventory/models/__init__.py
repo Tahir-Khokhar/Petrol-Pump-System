bf6a708f4a852cc1817593265375f7e61d03b477
Tahir-Khokhar <khokhartahir290@gmail.com>
2026-08-20 16:34:56 +0500

    Add remaining app files and configurations

---
+++ backend/apps/inventory/models/__init__.py
@@ -0,0 +1,4 @@
+from apps.inventory.models.inventory_item import InventoryItem
+from apps.inventory.models.inventory_transaction import InventoryTransaction
+
+__all__ = ['InventoryItem', 'InventoryTransaction']
