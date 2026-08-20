8ee7ca9da7e95ec2d25dc245abe3a6700a2e858f
Tahir-Khokhar <khokhartahir290@gmail.com>
2026-08-20 16:19:29 +0500

    Add remaining app files and configurations

---
+++ backend/apps/fuel/models/__init__.py
@@ -0,0 +1,5 @@
+from apps.fuel.models.fuel_type import FuelType  # noqa: F401
+from apps.fuel.models.fuel_price_history import FuelPriceHistory  # noqa: F401
+from apps.fuel.models.tank import Tank  # noqa: F401
+
+__all__ = ['FuelType', 'FuelPriceHistory', 'Tank']
